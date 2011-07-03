aede4e47851e197ae01ddfd4b93e30933e4909e0
Ujjwal Grover <ujj@ujj-laptop-2.local>
2011-07-03 18:51:43 +0530

    initial commit

---
+++ src/com/ujj/physicscheater/FormulaActivity.java
@@ -0,0 +1,58 @@
+package com.ujj.physicscheater;
+
+import com.ujj.physicscheater.R;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class FormulaActivity extends Activity {
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.formulaview);
+		Button closeButton = (Button) findViewById(R.id.close_button);
+		TextView tv = (TextView) findViewById(R.id.formulae);
+		Intent i = getIntent();
+		String field1Value = i.getStringExtra("topic");
+
+		if (field1Value.equals("Mechanics")) {
+			tv.setText(R.string.Mechanics);
+		} else if (field1Value.equals("Work, Energy, Power")) {
+			tv.setText(R.string.WPE);
+		} else if (field1Value.equals("Impulse, Momentum")) {
+			tv.setText(R.string.IM);
+		} else if (field1Value.equals("Rotational Motion")) {
+			tv.setText(R.string.RM);
+		} else if (field1Value.equals("Electromagnetic Waves")) {
+			tv.setText(R.string.EMW);
+		} else if (field1Value.equals("Optics")) {
+			tv.setText(R.string.OP);
+		} else if (field1Value.equals("Heat and Thermodynamics")) {
+			tv.setText(R.string.TD);
+		} else if (field1Value.equals("Electrostatics")) {
+			tv.setText(R.string.ES);
+		} else if (field1Value.equals("Electric Current")) {
+			tv.setText(R.string.EC);
+		} else if (field1Value.equals("Magnetism")) {
+			tv.setText(R.string.MG);
+		} else if (field1Value.equals("Gravitation")) {
+			tv.setText(R.string.G);
+		} else if (field1Value.equals("Important Constants")) {
+			tv.setText(R.string.C);
+		} else if (field1Value.equals("Fluids")) {
+			tv.setText(R.string.F);
+		}
+		// Log.d("FormulaTopic", field1Value);
+		closeButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				finish();
+			}
+		});
+	}
+}
---
+++ src/com/ujj/physicscheater/PhysicsCheater.java
@@ -0,0 +1,44 @@
+package com.ujj.physicscheater;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.view.View;
+import android.widget.AdapterView.OnItemClickListener;
+
+public class PhysicsCheater extends ListActivity {
+	/** Called when the activity is first created. */
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		String[] countries = getResources().getStringArray(
+				R.array.countries_array);
+		setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item,
+				countries));
+
+		ListView lv = getListView();
+		lv.setTextFilterEnabled(true);
+
+		lv.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				// When clicked, show a toast with the TextView text
+				Intent i = new Intent(PhysicsCheater.this,
+						FormulaActivity.class);
+				i.putExtra("topic", ((TextView) view).getText());
+				startActivity(i);
+				// Toast.makeText(getApplicationContext(), ((TextView)
+				// view).getText(),
+				// Toast.LENGTH_SHORT).show();
+
+			}
+		});
+	}
+}
